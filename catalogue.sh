20254995ef437db35a34110cc6389043d6bd3ab2
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-28 00:13:58 -0500

    cataloge using source

---
+++ catalogue.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ./common.sh # Source the common.sh file to use its functions
+app_name=catalogue # Define the application name
+
+app_setup  # Call the app setup function from common.sh
+nodejs_setup # Call the NodeJS setup function from common.sh
+systemd_setup # Call the systemd setup function from common.sh
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo # Copy the repo file
+VALIDATE $? "Adding MongoDB Repo" # Validate the last command
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE # Install MongoDB Shell
+VALIDATE $? "Installing MongoDB Shell"    # Validate the last command
+INDEX=$(mongosh mongodb.kalakoti.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE # Load the schema
+    VALIDATE $? "Loading $app_name Products to MongoDB"    # Validate the last command
+else
+    echo -e "$app_name Products already exists. $Y SKIPPING $N" # Print skipping message in yellow color
+fi  
+
+app_restart # Call the app restart function from common.sh
+print_total_time # Call the print total time function from common.sh
--- common.sh
+++ common.sh
@@ -10,6 +10,7 @@ LOGS_FOLDER="/var/log/shell-roboshop/"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 ) # Extract script name without extension
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # Define log file path
 START_TIME=$(date +%s) # Get the script start time
+MONGODB_HOST=mongodb.kalakoti.fun # MongoDB Host
 
 
 # -e enables the interpretation of backslash escapes
@@ -31,6 +32,58 @@ VALIDATE(){ # Functions receive arguments like normal scripts
     else
         echo -e "Installing $2 ... $G SUCCESSFUL $N" | tee -a $LOG_FILE # Print success message in green color
     fi 
+
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE # Disable the default nodejs module
+    VALIDATE $? "Disabling NodeJS module" # Validate the last command
+
+    dnf module enable nodejs:20 -y &>>$LOG_FILE # Enable the nodejs 20 module
+    VALIDATE $? "Enabling NodeJS 20 module" # Validate the last command
+
+    dnf install nodejs -y &>>$LOG_FILE # Install NodeJS
+    VALIDATE $? "Installing NodeJS"    # Validate the last command
+
+    npm install &>>$LOG_FILE # Download the application dependencies
+    VALIDATE $? "Installing Application Dependencies"    # Validate the last command
+}
+
+app_setup(){
+    mkdir -p /app #Create application directory if not exists 
+    VALIDATE $? "Creating Application Directory"    # Validate the last command
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE # Download the application code
+    VALIDATE $? "Downloading $app_name Application Code"    # Validate the last command
+
+    cd /app 
+    VALIDATE $? "Changing Directory to /app"    # Validate the last command
+
+    rm -rf /app/* # Remove any existing application code
+    VALIDATE $? "Removing the existing Application Code"    # Validate the last command
+
+    unzip /tmp/$app_name.zip &>>$LOG_FILE # Unzip the application code
+    VALIDATE $? "Unzipping $app_name Application Code"    # Validate the last command 
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service # Copy the service file
+    VALIDATE $? "Copying Service File"    # Validate the last command
+
+    systemctl daemon-reload # Reload systemd to register the service
+    VALIDATE $? "Reloading SystemD"    # Validate the last command
+
+    systemctl enable $app_name &>>$LOG_FILE # Enable the service
+    VALIDATE $? "Enabling $app_name Service"    # Validate the last command
+
+    systemctl start $app_name # Start the service
+    VALIDATE $? "Starting $app_name Service"    # Validate the last command
+
+}
+
+app_restart(){
+    systemctl restart $app_name # Restart the service
+    VALIDATE $? "Restarting $app_name Service"    # Validate the last command
 
 }
 
