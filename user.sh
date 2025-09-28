a51ac6098c7b43bdedcece7c50f1472e3ff35e8b
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-28 00:44:12 -0500

    user using source

---
+++ user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ./common.sh # Source the common.sh file to use its functions
+app_name=user # Define the application name
+
+app_setup  # Call the app setup function from common.sh
+nodejs_setup # Call the NodeJS setup function from common.sh
+systemd_setup # Call the systemd setup function from common.sh
+
+app_restart # Call the app restart function from common.sh
+print_total_time # Call the print total time function from common.sh
