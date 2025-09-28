03292d29ce5dbb9a447481345938f110f91e22b5
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-28 12:37:35 -0500

    Shipping

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -4,20 +4,21 @@ source ./common.sh
 
 check_root
 
-cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
-VALIDATE $? "Adding RabbitMQ repo"
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE # Copy the repo file
 
-dnf install rabbitmq-server -y &>>$LOG_FILE
-VALIDATE $? "Installing RabbitMQ Server"
+dnf install rabbitmq-server -y &>>$LOG_FILE # Install RabbitMQ
+VALIDATE $? "Installing RabbitMQ"    # Validate the last command
 
-systemctl enable rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "Enabling RabbitMQ Server"
+systemctl enable rabbitmq-server &>>$LOG_FILE # Enable RabbitMQ service
+VALIDATE $? "Enabling RabbitMQ"    # Validate the last command
 
-systemctl start rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "Starting RabbitMQ"
+systemctl start rabbitmq-server &>>$LOG_FILE # Start RabbitMQ service
+VALIDATE $? "Starting RabbitMQ"    # Validate the last command
 
-rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
-VALIDATE $? "Setting up permissions"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE # Add application user
+VALIDATE $? "Adding Application User to RabbitMQ"    # Validate the last command
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE # Set permissions to the application user
+VALIDATE $? "Setting Permissions to Application User"    # Validate the last command
 
 print_total_time
